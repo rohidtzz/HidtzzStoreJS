3a1e9076c7719197b516efaab6e87f15964b8ddd
rohidtzz <63803545+rohidtzz@users.noreply.github.com>
2024-06-09 15:53:22 +0700

    #

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layouts from '../../components/home/layout'
+export default function page() {
+  return (
+    <>
+    <Layouts>
+        <div className="container mx-auto">
+            <h1 className="text-3xl font-bold text-center">About Page</h1>
+        </div>
+    </Layouts>
+    </>
+  )
+}
